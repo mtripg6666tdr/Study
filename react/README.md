26ba98848aac9f4f69e69aab20ed8c7c17a065a8
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-09-10 23:46:16 +0900

    [react]初期コミット

---
+++ react/README.md
@@ -0,0 +1,2 @@
+# react
+Ref: https://qiita.com/TsutomuNakamura/items/72d8cf9f07a5a30be048
---
+++ react/webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path    = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "./src/index"),
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'ts-loader'
+        }]
+    }]
+  },
+  output: {
+    path: path.join(__dirname, "./dist/"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  }
+};
