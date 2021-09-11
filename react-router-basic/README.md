b01d5bc668d0b4b29b6d880a2e909fcd54078d28
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-09-11 22:07:00 +0900

    [react-router-basic]初期コミット

---
+++ react-router-basic/README.md
@@ -0,0 +1,2 @@
+# react-router-basic
+Ref: https://qiita.com/TsutomuNakamura/items/34a7339a05bb5fd697f2
---
+++ react-router-basic/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "ts-loader"
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  entry: path.join(__dirname, "./src/index"),
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "bundle.js"
+  }
+}
